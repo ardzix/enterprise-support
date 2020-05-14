5ef38b06f8fafc234e3db2af98ef5c1146b9baf2
Arif Dzikrullah <ardzyix@gmail.com>
2020-05-14 20:25:22 +0700

    vv

--- core/libs/bulk_upload.py
+++ core/libs/bulk_upload.py
@@ -143,11 +143,12 @@ def import_csv(file, uploader):
                     full_name=full_name,
                     email=email
                 )
-            
-            profile, created = Profile.objects.get_or_create(
-                created_by=user,
-            )
-            profile.save()
+            try:
+                profile, created = Profile.objects.get_or_create(
+                    created_by=user,
+                )
+            except:
+                profile = Profile.objects.filter(created_by=user).last()
         else:
             user = profile.owned_by
 
