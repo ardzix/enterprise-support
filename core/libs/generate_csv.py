78c0c1a1c4f5fef7db9d784a047e18a26833ea03
dpht <dannyputraht@gamil.com>
2020-05-21 22:38:49 +0700

    modified generate csv

--- core/libs/generate_csv.py
+++ core/libs/generate_csv.py
@@ -7,8 +7,13 @@ def generate_csv(data, filename, headers, start_time, end_time):
     """
     data => List of list [[],[],[]]
     """
+    if not start_time and not end_time:
+        date = datetime.utcnow().strftime("%Y-%M-%d")
+    else:
+        date = f'{start_time}_{end_time}'
+
     response = HttpResponse(content_type='text/csv')
-    filename = f'{filename}_{start_time}_{end_time}'
+    filename = f'{filename}_{date}'
     response['Content-Disposition'] = f'attachment; filename="{filename}.csv"'
     writer = csv.writer(response)
     writer.writerow(headers)
